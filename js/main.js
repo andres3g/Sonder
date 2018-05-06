e22efb7f70f30ac1ea53e91765d9c718b67624a5
Ninguno <Ninguno@MacBook-de-Ninguno.local>
2018-05-06 17:24:14 -0500

    v1.2

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', function()
 {
-  // var x = document.getElementById("boton1").onclick = hola;
-  //
-  // function hola() {
-  //   alert("Funcionando...");
-  // }
-
 
   var sonderBtm = document.getElementById('artista');
     var addBtn = document.getElementById('nombre');
@@ -22,6 +16,15 @@ document.addEventListener('DOMContentLoaded', function()
 
         console.log(ydoc);
 
+        if (url.includes("youtube.com/watch?v")) {
+
+          var sonderTitle = document.getElementById('title');
+          var sonderArtist = document.getElementById('artist');
+          var babeAlbum = document.getElementById('album');
+
+          var pageUrl = tab[0].url;
+          var pageTitle = tab[0].title;
+
         if (url.includes("youtube.com/watch?v"))
         {
           var regex;
@@ -29,9 +32,62 @@ document.addEventListener('DOMContentLoaded', function()
           var newTitle = titleArray[1];
           var newArtist = titleArray[0];
 
+          if(pageTitle.indexOf(')')>-1){
+            regex = / *\([^)]*\) */g;
+            newTitle = newTitle.replace(regex, " ").trim();
+            newArtist = newArtist.replace(regex, " ").trim();
+          }
+          if(pageTitle.indexOf('[')>-1)
+          {
+              regex = / *\[[^)]*\] */g;
+              newTitle = newTitle.replace(regex, " ").trim();
+              newArtist = newArtist.replace(regex, " ").trim();
+          }
+
+          regex = /"/g
+          if(newTitle.indexOf('"')>-1)
+              newTitle = newTitle.replace(regex,"").trim();
+
+          if(newArtist.indexOf('"')>-1)
+              newArtist = newTitle.replace(regex,"").trim();
+
+              //aqui voy
+              sonderTitle.value = newTitle;
+              sonderArtist.value = newArtist;
 
 
-        }
+        }else
+          //falta
+
+          var id = url.substring(url.lastIndexOf("watch?v=") + "watch?v=".length,url.length);
+
+          if(id.includes("&"))
+            id = id.substring(0, id.indexOf("#"));
+
+            sonderBtm.addEventListener('click', function (){
+
+              pageUrl = tab[0].url.replace("- YouTube", "");
+
+              var title = document.getElementById('title').value;
+              var artist = document.getElementById('artist').value;
+              var album = document.getElementById('album').value;
+              var playlist = document.getElementById("playlist").value;
+
+              var json = {
+                  title: title.trim(),
+                  artist: artist.trim(),
+                  album: album.trim(),
+                  playlist: playlist.trim(),
+                  sonder: 1,
+                  id: id.trim(),
+                  page: pageTitle.trim()
+
+              }
+
+              sendData(json);
+            });
+
+      }
         else{
           document.getElementById("warning").innerHTML += "El URL no pertenece a una cancion de YouTube";
 
